2da5cc324d796426ff0231e92c10a654f26e7152
Mikael Berg <mikael@zeppelin.no>
2017-03-14 10:36:38 +0100

    try outputting info

--- k8s/authenticate.sh
+++ k8s/authenticate.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-kubectl config set-cluster cluster --server=${K8S_ENDPOINT}
+kubectl config set-cluster cluster --server=${K8S_ENDPOINT} --insecure-skip-tls-verify
 kubectl config set-credentials cluster-admin --username=${K8S_USERNAME} --password ${K8S_PASSWORD}
 
 kubectl config set-context ci --cluster=cluster --user=cluster-admin
 kubectl config use-context ci
+
+kubectl cluster-info
