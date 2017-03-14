8f7e9a6d56439093eddb6062b7d65badc1c1dfdb
Mikael Berg <mikael@zeppelin.no>
2017-03-14 09:51:35 +0100

    try to authenticate with cluster

---
+++ k8s/authenticate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+~/.kube/kubectl config set-cluster cluster --server=${K8S_ENDPOINT}
+~/.kube/kubectl config set-credentials cluster-admin --username=${K8S_USERNAME} --password ${K8S_PASSWORD}
+
+~/.kube/kubectl config set-context ci --cluster=cluster --user=cluster-admin
+~/.kube/kubectl use-context ci
