6eca55456ba6e1b67f1589923b782db14a24b0a9
Mikael Berg <mikael@zeppelin.no>
2017-03-14 14:27:50 +0100

    fix typos

--- docker/build.sh
+++ docker/build.sh
@@ -4,7 +4,7 @@
 if [[ -e ~/docker/image.tar ]]; then docker load -i ~/docker/image.tar; fi
 
 docker build --rm=false -t ${DOCKER_TAG_NAME} .
-docker tag -f ${DOCKER_TAG_NAME}:latest ${DOCKER_TAG_NAME}:${DOCKER_TAG_SHA}
+docker tag -f ${DOCKER_TAG_NAME}:latest ${DOCKER_TAG_NAME}:${DOCKER_SHA_TAG}
 
 # Store cache
 mkdir -p ~/docker; docker save ${DOCKER_TAG_NAME} > ~/docker/image.tar
