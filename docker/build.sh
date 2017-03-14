4767a6fca15253477072fc8fda62ef039e5af5ed
Mikael Berg <mikael@zeppelin.no>
2017-03-14 12:52:57 +0100

    assume docker tag sha comes from circle, dont export it

--- docker/build.sh
+++ docker/build.sh
@@ -3,10 +3,8 @@
 # Load cache if it exists
 if [[ -e ~/docker/image.tar ]]; then docker load -i ~/docker/image.tar; fi
 
-export DOCKER_SHA_TAG=${CIRCLE_SHA1:0:7}
-
 docker build --rm=false -t ${DOCKER_TAG_NAME} .
-docker tag -f ${DOCKER_TAG_NAME}:latest ${DOCKER_TAG_NAME}:${DOCKER_SHA_TAG}
+docker tag -f ${DOCKER_TAG_NAME}:latest ${DOCKER_TAG_NAME}:${DOCKER_TAG_SHA}
 
 # Store cache
 mkdir -p ~/docker; docker save ${DOCKER_TAG_NAME} > ~/docker/image.tar
