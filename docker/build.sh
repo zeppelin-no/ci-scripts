1663e20cd2a393f23ca1ac8eaecba4050d673f60
Mikael Berg <mikael@zeppelin.no>
2017-03-15 09:49:32 +0100

    Disable docker cache

--- docker/build.sh
+++ docker/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Load cache if it exists
-if [[ -e ~/docker/image.tar ]]; then docker load -i ~/docker/image.tar; fi
+# @NOTE: Trying to use Docker cache with CircleCI 1.0 seems to do more harm
+# than good. Disabling for now.
+# if [[ -e ~/docker/image.tar ]]; then docker load -i ~/docker/image.tar; fi
 
-docker build --rm=false -t ${DOCKER_TAG_NAME} .
+docker build -t ${DOCKER_TAG_NAME} .  # add --rm=false when using docker cache
 docker tag -f ${DOCKER_TAG_NAME}:latest ${DOCKER_TAG_NAME}:${DOCKER_SHA_TAG}
 
 # Store cache
-mkdir -p ~/docker; docker save ${DOCKER_TAG_NAME} > ~/docker/image.tar
+# mkdir -p ~/docker; docker save ${DOCKER_TAG_NAME} > ~/docker/image.tar
