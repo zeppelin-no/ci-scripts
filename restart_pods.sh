a3bc1e21d6f8ba89e2632d09820a499863432e59
Stein Strindhaug <stein@zeppelin.no>
2017-08-04 14:17:34 +0200

    Add optional script to restart pods for a stateful set

---
+++ restart_pods.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Restart pods for a stateful set by deleting the old ones.
+#
+# Takes two ordered arguments:
+#   - appname     The name of the stateful set.
+#   - namespace   The kubernetes namespace to use.
+#
+#  Example: to restart the pods for the stateful set "drebbel":
+#  ./restart_pods.sh drebbel prod
+
+APP_NAME=$1
+K8S_NAMESPACE=$2
+
+echo "Deleting pods labeled app:${APP_NAME} in namespace ${K8S_NAMESPACE}..."
+kubectl delete pods -l app=${APP_NAME} --namespace=${K8S_NAMESPACE}
+echo "Pods restarting."
