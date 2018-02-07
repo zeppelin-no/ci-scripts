16dc88476688659ce3facc317ad46807dc7631a5
andemork <ajmoerk@gmail.com>
2018-02-07 09:42:23 +0100

    use correct variables

--- k8s/authenticate.sh
+++ k8s/authenticate.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 ENDPOINT=${K8S_ENDPOINT}
-USERNAME=${K8S_ENDPOINT}
-PASSWORD=${K8S_ENDPOINT}
+USERNAME=${K8S_USERNAME}
+PASSWORD=${K8S_PASSWORD}
 
 if [ "$1" = "dev" ]; then
   ENDPOINT=${K8S_ENDPOINT_DEV}
-  USERNAME=${K8S_ENDPOINT_DEV}
-  PASSWORD=${K8S_ENDPOINT_DEV}
+  USERNAME=${K8S_USERNAME_DEV}
+  PASSWORD=${K8S_PASSWORD_DEV}
 fi
 
-kubectl config set-cluster cluster --server=ENDPOINT --insecure-skip-tls-verify
-kubectl config set-credentials cluster-admin --username=USERNAME --password PASSWORD
+kubectl config set-cluster cluster --server=${ENDPOINT} --insecure-skip-tls-verify
+kubectl config set-credentials cluster-admin --username=${USERNAME} --password ${PASSWORD}
 
 kubectl config set-context ci --cluster=cluster --user=cluster-admin
 kubectl config use-context ci
